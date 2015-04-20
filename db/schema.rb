1030261c8eeff91bf779188d933f10229cd940b1
Chen Liu <qliu2016@kellogg.northwestern.edu>
2015-04-20 13:13:04 -0500

    HW3

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "assignments", force: true do |t|
+    t.string  "name"
+    t.integer "teacher_id"
+    t.integer "num_assignments"
+    t.string  "due_date"
+    t.integer "estimate_time"
+  end
+
+  add_index "assignments", ["teacher_id"], name: "index_assignments_on_teacher_id"
+
+  create_table "assistants", force: true do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject_pref"
+    t.string "grade_pref"
+    t.string "password"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer "assistant_id"
+    t.string  "date"
+    t.integer "stars"
+    t.text    "review"
+  end
+
+  add_index "reviews", ["assistant_id"], name: "index_reviews_on_assistant_id"
+
+  create_table "teachers", force: true do |t|
+    t.string "name"
+    t.string "school"
+    t.string "subject"
+    t.string "grade"
+    t.string "password"
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,40 @@
+# Deletes everything from the database so that you start fresh
+puts "Deleting all records from the database..."
+Teacher.delete_all
+Assignment.delete_all
+Assistant.delete_all
+Review.delete_all
+
+# Create the teachers
+puts "Creating teachers..."
+katelyn = Teacher.create(name: "Katelyn Coleman", school: "Brooklyn Preparatory High School", subject: "Math", grade: "10", password: "squirtle123")
+beth = Teacher.create(name: "Beth Rich", school: "Rowe Elementary School", subject: "Special Ed", grade: "3", password: "pikachu123")
+chen = Teacher.create(name: "Chen Liu", school: "Brooklyn Preparatory High School", subject: "Science", grade: "10", password: "jigglypuff123")
+siby = Teacher.create(name: "Siby Phillips", school: "Brooklyn Preparatory High School", subject: "English", grade: "9", password: "charmander123")
+david = Teacher.create(name: "David Studer", school: "Khalil Gibran High School", subject: "Science", grade: "8", password: "golem123")
+
+# Create the assignments
+puts "Creating assignments..."
+assignment1 = Assignment.create(name: "Math HW1", teacher_id: katelyn.id, num_assignments: "100", due_date: "May 1, 2015", estimate_time: "2")
+assignment2 = Assignment.create(name: "Science Lab 1", teacher_id: chen.id, num_assignments: "120", due_date: "May 5, 2015", estimate_time: "6")
+assignment3 = Assignment.create(name: "Exam 1", teacher_id: chen.id, num_assignments: "150", due_date: "May 10, 2015", estimate_time: "4")
+assignment4 = Assignment.create(name: "Worksheet 1", teacher_id: beth.id, num_assignments: "15", due_date: "May 2, 2015", estimate_time: "1")
+assignment5 = Assignment.create(name: "English Essay 2", teacher_id: siby.id, num_assignments: "30", due_date: "June 1, 2015", estimate_time: "5")
+
+# Create the assistants
+puts "Creating assistants..."
+alex = Assistant.create(name: "Alex", email: "a123@gmail.com", subject_pref: "science", grade_pref: "9-12", password: "mew123")
+barbara = Assistant.create(name: "Barbara", email: "b123@gmail.com", subject_pref: "social studies", grade_pref: "6-8", password: "bulbasaur123")
+catherine = Assistant.create(name: "Catherine", email: "c123@gmail.com", subject_pref: "science", grade_pref: "6-8", password: "metapod123")
+donald = Assistant.create(name: "Donald", email: "d123@gmail.com", subject_pref: "english", grade_pref: "9-12", password: "blastoise123")
+evan = Assistant.create(name: "Evan", email: "e123@gmail.com", subject_pref: "math", grade_pref: "9-12", password: "ivysaur123")
+
+# Create the reviews
+puts "Creating reviews..."
+review1 = Review.create(assistant_id: alex.id, date: "April 15 2015", stars: "5", review: "Great job!")
+review2 = Review.create(assistant_id: barbara.id, date: "April 13 2015", stars: "4", review: "Good but late")
+review3 = Review.create(assistant_id: barbara.id, date: "April 11 2015", stars: "1", review: "Terrible!")
+review4 = Review.create(assistant_id: donald.id, date: "April 15 2015", stars: "5", review: "Awesome!")
+review5 = Review.create(assistant_id: catherine.id, date: "April 20 2015", stars: "4", review: "Pretty good.")
+
+puts "There are now #{Teacher.count} teachers, #{Assistant.count} assistants, #{Assignment.count} assignments, and #{Review.count} reviews in the database."
