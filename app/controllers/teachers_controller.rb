32ac32788cfc7315e60ebb0e692916fd9763cdab
Chen Liu <qliu2016@kellogg.northwestern.edu>
2015-05-04 15:14:09 -0500

    HW5

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,4 +6,29 @@ class TeachersController < ApplicationController
   def show
   end
 
+  def new
+    @teacher = Teacher.new
+  end
+
+  def create
+  	Teacher.create(params["teacher"])
+  	redirect_to teachers_url
+  end
+
+  def edit
+  	@teacher = Teacher.find_by(id: params["id"])
+  end
+
+  def update
+  	@teacher = Teacher.find_by(id: params["id"])
+  	@teacher.update(params["teacher"])
+  	redirect_to teachers_url  
+  end
+
+  def destroy
+  	@teacher = Teacher.find_by(id: params["id"])
+  	@teacher.delete
+  	redirect_to teachers_url
+  end
+
 end
